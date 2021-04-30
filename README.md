534e26800a138a2b564380a061984c9baf41bedf
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2021-04-30 17:28:10 +0200

    :sparkles: init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Custom 500 page
+
+A custom 500 page for my VPS where I host my projects
+
+## Screenshot
+
+![screenshot](./screenshot.png)
---
+++ script.js
@@ -0,0 +1,4 @@
+const siteName = document.getElementById("site_name");
+
+// Set the name of the website
+siteName.textContent = location.hostname;
